17956aac175298a715fca4aa4f8bedfdf9b5e639
eamarquezh <supericman@outlook.com>
2024-02-15 10:06:16 -0600

    act

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Cierra el menú al hacer clic en un enlace
+    var links = document.querySelectorAll('.navbar-nav a');
+    links.forEach(function (link) {
+        link.addEventListener('click', function () {
+            var navbar = document.querySelector('.navbar-collapse');
+            var bsCollapse = new bootstrap.Collapse(navbar);
+            bsCollapse.hide();
+        });
+    });
+});
